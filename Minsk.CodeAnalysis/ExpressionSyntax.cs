884105bbfa961d3613ab9252fe2911001671c977
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-07-27 01:14:15 +1000

    Extract all logic to separate files

---
+++ Minsk.CodeAnalysis/ExpressionSyntax.cs
@@ -0,0 +1,5 @@
+namespace Minsk.CodeAnalysis;
+
+public abstract class ExpressionSyntax : SyntaxNode
+{
+}
---
+++ Minsk.CodeAnalysis/SyntaxKind.cs
@@ -0,0 +1,18 @@
+namespace Minsk.CodeAnalysis;
+
+public enum SyntaxKind
+{
+    NumberToken,
+    WhitespaceToken,
+    PlusToken,
+    MinusToken,
+    StarToken,
+    SlashToken,
+    OpenParenthesisToken,
+    CloseParenthesisToken,
+    BadToken,
+    EndOfFileToken,
+    NumberExpression,
+    BinaryExpression,
+    ParenthesizedExpression
+}

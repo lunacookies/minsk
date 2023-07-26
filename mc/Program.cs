efe5646fab2492030597f1003edddd8fd5b07817
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-07-27 01:00:57 +1000

    Add method on `SyntaxTree` to parse

--- mc/Program.cs
+++ mc/Program.cs
@@ -18,8 +18,7 @@ internal class Program
                 return;
             }
 
-            var parser = new Parser(line);
-            var syntaxTree = parser.Parse();
+            var syntaxTree = SyntaxTree.Parse(line);
 
             var color = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Gray;
@@ -308,6 +307,12 @@ internal sealed class SyntaxTree
     public IReadOnlyList<string> Diagnostics { get; }
     public ExpressionSyntax Root { get; }
     public SyntaxToken EndOfFileToken { get; }
+
+    public static SyntaxTree Parse(string text)
+    {
+        var parser = new Parser(text);
+        return parser.Parse();
+    }
 }
 
 internal class Parser
